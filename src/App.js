d79486976921012922d9625932d748d75bdbbca6
Aleh Olivira <cupoftea@gmail.com>
2023-06-30 16:01:17 -0300

    add initial animation

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import Footer from "./components/Footer";
 import CenterArea from "./components/CenterArea";
 import Header from "./components/Header";
 import Rip from "./components/Rip";
+import { useEffect, useState } from "react";
+import InitialScreen from "./components/InitialScreen";
 
 function App() {
   const getLife = useSelector((state) => state.setPokeInfoReducer.life);
+  const [isIntro, setIsIntro] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsIntro(false);
+    }, 14000);
+  }, []);
   return (
     <>
       {getLife > 0 ? (
         <div className="App">
-          <Header />
-          <CenterArea />
-          <Footer />
+          {isIntro ? (
+            <InitialScreen />
+          ) : (
+            <>
+              <Header />
+              <CenterArea />
+              <Footer />
+            </>
+          )}
         </div>
       ) : (
         <Rip />
---
+++ src/components/InitialScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./InitialScreen.css";
+
+function InitialScreen() {
+  return <div className="initial-screen"></div>;
+}
+
+export default InitialScreen;

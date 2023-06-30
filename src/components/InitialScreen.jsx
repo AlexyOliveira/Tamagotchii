f503a3001ee334b3b9796516a8fd1038b66d3a2d
Aleh Olivira <cupoftea@gmail.com>
2023-06-30 16:42:21 -0300

    ..

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -6,7 +6,7 @@ import { setEnergySub, setHungerSub } from "../redux/actions";
 function InitialScreen() {
     const dispatch = useDispatch()
     useEffect(() => {
-        let times = 4
+        let times = 3
         while (times > 0) {
             dispatch(setHungerSub())
             times -= 1

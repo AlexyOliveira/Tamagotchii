243b9f74ad3d1c7eb8b453d6dc4ceeb80c8d591b
Aleh Olivira <cupoftea@gmail.com>
2023-06-30 16:45:16 -0300

    ..

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -4,15 +4,14 @@ import { useDispatch } from "react-redux";
 import { setHungerSub } from "../redux/actions";
 
 function InitialScreen() {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        let times = 3
-        while (times > 0) {
-            dispatch(setHungerSub())
-            times = times - 1
-        }
-        
-    }, [dispatch])
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let times = 4;
+    while (times > 0) {
+      dispatch(setHungerSub());
+      times -= 1;
+    }
+  }, [dispatch]);
   return <div className="initial-screen"></div>;
 }
 
